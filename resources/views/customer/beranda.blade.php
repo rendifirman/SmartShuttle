2aec3b7aea3c505f0a67504013df6492602300e8
Muhamad Faqih <mfaqihfeb17@gmail.com>
2026-01-20 13:19:53 +0700

    typo

--- resources/views/customer/beranda.blade.php
+++ resources/views/customer/beranda.blade.php
@@ -2857,7 +2857,7 @@
 
                 <div class="form-notice">
                     <i class="fas fa-info-circle"></i>
-                    Review Anda akan membantu kami meningkatkan layanan
+                    Review Anda akan membantu kami meningkatkan layana
                 </div>
             </div>
         </div>
