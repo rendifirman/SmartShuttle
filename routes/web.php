fc57d3164aef23abe1fc872d3efc3c9e6ff6b505
Rendi Firmansyah <rf584978@gmail.com>
2026-01-23 09:48:57 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -166,7 +166,7 @@ Route::delete('/customer/avatar/delete', [CustomerController::class, 'deleteAvat
 // ============================================================
 
 // ★★★ ROUTES YANG BUTUH LOGIN ★★★
-Route::middleware(['auth.customer'])->group(function () {
+Route::middleware(['auth:web'])->group(function () {
 
     // ★★★ PROFIL & DASHBOARD ★★★
     Route::get('/customer/dashboardprofile', [CustomerController::class, 'profil'])->name('customer.dashboardprofile');
